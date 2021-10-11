fea8d6a00d7072b6492b816efe6e119a6531a78b
yedhu <yedhubenny@gmail.com>
2021-10-11 15:02:09 +0530

    Added the entire app

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -3,6 +3,7 @@ package guru.springframework.spring5webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/** veruthe **/
 @SpringBootApplication
 public class Spring5webappApplication {
 
